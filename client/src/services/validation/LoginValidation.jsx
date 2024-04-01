eaaa85968a3c25bbd76940ecd48cb5acda4cf15a
Muhammed Dilshad A <157797397+dilshadanikkadan@users.noreply.github.com>
2024-04-01 13:31:29 +0530

    Delete client directory

--- client/src/services/validation/LoginValidation.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import * as Yup from 'yup'
-
-const regex = /^(?=.*[a-zA-Z])(?=.*[0-9]).+$/;
-export const LoginValidation = Yup.object({
-    email: Yup.string().email('please enter a valid email').required("please enter email"),
-    password: Yup.string().matches(regex, "Password should include lettters and numbers").required("please fill password"),
-})
